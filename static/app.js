884101b6b6bc4260a6a6ff29f6d3aad24b40f9c7
Chris Whalen <chris@chriswhalen.ca>
2021-01-26 16:22:09 -0400

    js fix

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 
 const bg =()=> document.querySelector('body').style.background = `hsla(${Math.random()*255}, 60%, 20%, 1)`
 
-setInterval(bg, 15000)
+setInterval(bg, 10000)
 
-bg()
+document.addEventListener('DOMContentLoaded', bg)
