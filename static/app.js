29057f04d6f847d8474e8cf7d3e96c6df92c91ae
Chris Whalen <chris@chriswhalen.ca>
2021-01-28 19:11:10 -0400

    updates

--- static/app.js
+++ static/app.js
@@ -1,6 +1,9 @@
 
+const a =()=> document.querySelectorAll('a').forEach((tag)=> tag.setAttribute('target', '_blank'))
+
 const bg =()=> document.querySelector('body').style.background = `hsla(${Math.random()*255}, 60%, 20%, 1)`
 
 setInterval(bg, 10000)
 
+document.addEventListener('DOMContentLoaded', a)
 document.addEventListener('DOMContentLoaded', bg)
